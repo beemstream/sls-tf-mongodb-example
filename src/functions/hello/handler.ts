14c183109801bcac01784656033e4ad43d5773b5
BeemStream <beemstreamofficial@gmail.com>
2022-03-08 21:18:26 +0000

    feat: add retryWrites: false

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -3,14 +3,20 @@ import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import { MongoClient } from 'mongodb';
 import schema from './schema';
+import pino from 'pino-lambda';
+const logger = pino();
 
-const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  const client = new MongoClient(process.env.DB_CONNECTION_STRING);
-  await client.connect();
+const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event, context) => {
+  logger.withRequest(event, context);
+
+  const client = new MongoClient(process.env.DB_CONNECTION_STRING, { retryWrites: false });
+  const connected = await client.connect();
+
+  const result = await connected.db().collection('settings').insertOne({ foo: 'bar' });
+
   console.log('Connected successfully');
   return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-    event,
+    result
   });
 };
 
